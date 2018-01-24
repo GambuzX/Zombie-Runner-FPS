fa2adbdf696bdc0d8540d996fa03aeb75b87bec7
Mário Gil <mg_mesquita@hotmail.com>
2018-01-24 15:40:32 +0000

    Character scaling and spawn points

---
+++ Assets/Player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public Transform spawnPointsParent;
+    public bool reSpawn = false;
+
+    private Transform[] spawnPoints;
+    private bool lastToggle = false;
+
+    void Start()
+    {
+        spawnPoints = spawnPointsParent.GetComponentsInChildren<Transform>();
+    }
+
+    private void Update()
+    {
+        if (lastToggle != reSpawn)
+        {
+            ReSpawn();
+            reSpawn = false;
+        } else
+        {
+            lastToggle = reSpawn;
+        }
+    }
+
+    public void ReSpawn()
+    {
+        Transform spawnPoint = spawnPoints[Random.Range(1, spawnPoints.Length)];
+        this.transform.position = spawnPoint.position;
+    }
+}
