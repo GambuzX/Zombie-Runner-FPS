e477c4b9843a1e5a2ae8f329c5dc42af8918badb
Mário Gil <mg_mesquita@hotmail.com>
2018-01-24 18:13:07 +0000

    Input mapping

---
+++ Assets/Scripts/Helicopter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Helicopter : MonoBehaviour {
+
+    public AudioClip callSound;
+
+    private AudioSource audioSource;
+    private bool called = false;
+
+	void Start () {
+        audioSource = GetComponent<AudioSource>();
+	}
+	
+
+	void Update () {
+		if (Input.GetButtonDown("CallHeli") && !called)
+        {
+            audioSource.Play();
+            called = true;
+        }
+	}
+}
