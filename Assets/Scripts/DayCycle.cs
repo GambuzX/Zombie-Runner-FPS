4b16ea05a0af982d35fe6a730783477b679f77d9
Mário Gil <mg_mesquita@hotmail.com>
2018-01-26 11:07:47 +0000

    Day/Night cycle and detecting clear area
    Clear area for helicopter landing

---
+++ Assets/Scripts/DayCycle.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayCycle : MonoBehaviour {
+
+    [Tooltip ("Number of minutes per second that pass, try 60")]
+    public float minutesPerSecond;
+
+    private Quaternion startRotation;
+
+    private void Start()
+    {
+        startRotation = transform.rotation;
+    }
+
+    private void Update()
+    {
+        float angleThisFrame = Time.deltaTime / 360 * minutesPerSecond;
+        transform.RotateAround(transform.position, Vector3.forward, angleThisFrame);
+    }
+}
---
+++ Assets/Scripts/LandingAreaDetector.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LandingAreaDetector : MonoBehaviour {
+
+    private bool clear = false;
+    private bool soundPlayed = false;
+    public float timeSinceLastTrigger = 0f;
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }    
+
+	void Update () {
+        timeSinceLastTrigger += Time.deltaTime;
+
+        if (timeSinceLastTrigger >= 1 && !soundPlayed)
+        {
+            clear = true;
+            soundPlayed = true;
+            audioSource.Play();
+        }
+	}
+
+    void OnTriggerStay()
+    {
+        timeSinceLastTrigger = 0f;
+        soundPlayed = false;
+    }
+}
